8e896c782784bc8debace35788f548a6348e902c
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-13 13:12:29 +0200

    Create  HomeSection component and add dummy data and images for testing.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,36 @@
-// 
+export const dummyCardData = [
+    {
+        id: '1',
+        image: '/images/2.avif',
+        title: 'Hiking',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur, ipsa laboriosam porro vitae magnam consectetur earum tempora neque quaerat!'
+    },
+    {
+        id: '2',
+        image: '/images/3.avif',
+        title: 'Some title',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur, ipsa laboriosam porro vitae magnam consectetur earum tempora neque quaerat!'
+    },
+    {
+        id: '3',
+        image: '/images/4.avif',
+        title: 'Other title',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur, ipsa laboriosam porro vitae magnam consectetur earum tempora neque quaerat!'
+    },
+    {
+        id: '4',
+        image: '/images/5.avif',
+        title: 'Title',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum tenetur, ipsa laboriosam porro vitae magnam consectetur earum tempora neque quaerat!'
+    }
+];
+
+
+export const homeTopImages = [
+    '/images/5.avif',
+    '/images/2.avif',
+    '/images/3.avif',
+    '/images/6.avif',
+    '/images/1.avif',
+    '/images/4.avif'
+];
