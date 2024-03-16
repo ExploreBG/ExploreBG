163d39c535426eb741035f529fbd47b492681672
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-16 19:06:14 +0200

    Logo, navigation and welcome section improvements.

--- src/components/common/CLogo/CLogo.tsx
+++ src/components/common/CLogo/CLogo.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import './CLogo.scss';
 
 interface CLogoProps { }
 
 const CLogo: React.FC<CLogoProps> = () => {
+    const currentPath = usePathname();
+    const [oldPath, setOldPath] = useState<string>('');
+
+    useEffect(() => {
+        if (currentPath != oldPath) {
+            const timer = setTimeout(() => {
+                setOldPath(currentPath);
+            }, 2000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [currentPath, oldPath]);
+
     return (
         <Link href={'/'}>
             <figure className="logo">
-                <figcaption>Explore BG</figcaption>
+                <figcaption>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>E</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>x</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>p</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>l</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>o</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>r</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>e</span>
+                    <span>&nbsp;</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>B</span>
+                    <span className={oldPath != currentPath ? 'rolling' : ''}>G</span>
+                </figcaption>
                 <Image
                     src="/logo.png"
                     width={40} height={40} loading="eager"
