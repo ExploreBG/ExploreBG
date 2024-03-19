b98c8dea600d3f6bce3723bb9384ba9cba696305
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-19 16:33:33 +0200

    Update next-intl middleware and main layout.tsx

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
 import createMiddleware from 'next-intl/middleware';
+import { pathnames, locales, localePrefix } from './config';
 
 export default createMiddleware({
-    locales: ['bg', 'en'],
-    defaultLocale: 'bg'
+    locales,
+    defaultLocale: 'bg',
+    pathnames,
+    localePrefix
 });
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(bg|en)/:path*']
+    matcher: [
+        // Enable a redirect to a matching locale at the root
+        '/',
+
+        // Set a cookie to remember the previous locale for
+        // all requests that have a locale prefix
+        '/(bg|en)/:path*',
+
+        // Enable redirects that add missing locales
+        // (e.g. `/pathnames` -> `/en/pathnames`)
+        '/((?!_next|_vercel|.*\\..*).*)'
+    ]
 };
