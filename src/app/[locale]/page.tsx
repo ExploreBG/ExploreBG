74f2196750ff771b63df7fa4ff415242676effd2
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-20 23:20:22 +0200

    Add 'en' and 'bg' json files for dictionaries and add translation to Home page.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,21 +1,27 @@
-'use client';
-
 import React from 'react';
 import Image from 'next/image';
+import { useTranslations } from 'next-intl';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 import './home.scss';
 import IntersectionObserverComponent from '@/components/IntersectionObserverComponent';
 import HomeSection from '@/components/homeSection/HomeSection';
 import { dummyCardData, homeTopImages } from '@/utils/utils';
 
-interface HomeProps { }
+interface HomeProps {
+    params: { locale: string }
+}
 
-const Home: React.FC<HomeProps> = () => {
+const Home: React.FC<HomeProps> = ({ params: { locale }}) => {
+    unstable_setRequestLocale(locale);
+
+    const t = useTranslations('home');
+
     return (
         <main className="home">
             <section className="home__top">
-                <h1>Looking for adventures or just want to visit a new place</h1>
-                <h3>ExploreBG is the perfect choice</h3>
+                <h1>{t('section-top.h1')}</h1>
+                <h2>{t('section-top.h2')}</h2>
 
                 <figure className="home__top__images">
                     {homeTopImages.map((img) => (
@@ -29,20 +35,20 @@ const Home: React.FC<HomeProps> = () => {
                 </figure>
 
                 <blockquote>
-                    <p>If we were meant to stay in one place, we&apos;d have roots instead of feet.</p>
-                    <span>- Rachel Wolchin</span>
+                    <p>{t('section-top.quote.text')}</p>
+                    <span>- {t('section-top.quote.author')}</span>
                 </blockquote>
             </section>
 
             <section className="home__section-wrapper">
-                <h2 className="hidden">The Devil&apos;s bridge</h2>
-                <p className="hidden">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur minima natus delectus temporibus, voluptatibus eveniet ut minus alias cumque soluta adipisci repudiandae at, vel quos quod impedit repellendus praesentium voluptatum.</p>
+                <h2 className="hidden">{t('section-2.title')}</h2>
+                <p className="hidden">{t('section-2.text')}</p>
             </section>
 
             <section className="home__section-buffer"></section>
 
             <HomeSection
-                title='Discover unforgettable destinations'
+                title={t('section-destination.title')}
                 name='destinations'
                 data={dummyCardData}
             />
