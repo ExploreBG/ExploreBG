4463459b428aef8406f1974f2c17b0e9d22657f6
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-22 19:14:02 +0200

    Update 'Loading'.

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
-const Loading = () => {
+interface LoadingProps {
+    params: { locale: string }
+}
+
+const Loading: React.FC<LoadingProps> = ({ params }) => {
+    const locale = params?.locale;
+    unstable_setRequestLocale(locale);
+    
+    const t = useTranslations('loading');
+    const text = t('text');
+
     return (
-        <main>
-            <h2>Loading...</h2>
+        <main className="loading">
+            <h1>
+                {text.split('').map((l, index) => (
+                    <span key={index}>{l}</span>
+                ))}
+                <span>.</span>
+                <span>.</span>
+                <span>.</span>
+            </h1>
         </main>
     );
 };
