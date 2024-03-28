14800cecaa3afc47ad6ef4eafd6a00847a2605cb
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-03-28 21:10:19 +0200

    Add 'Trail info' section in 'Hike details' page.

--- src/app/[locale]/hikes/[hikeId]/page.tsx
+++ src/app/[locale]/hikes/[hikeId]/page.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { IoLocationOutline } from 'react-icons/io5';
 import { BsCalendar2Date } from 'react-icons/bs';
+import { GrMapLocation } from 'react-icons/gr';
+import { GiPathDistance, GiMountainRoad } from 'react-icons/gi';
 
 import { Link } from '@/navigation';
 import { dummyHike } from '@/utils/utils';
@@ -58,7 +60,31 @@ const HikeDetails: React.FC<HikeDetailsProps> = ({ params: { locale } }) => {
                 </section>
 
                 <section className="hike-details__trail-info">
-                    <p>Trail info</p>
+                    <div className="hike-details__trail-info__pair">
+                        <details open>
+                            <summary>From: <strong>Prava polyna</strong></summary>
+                            <GrMapLocation />&nbsp; 018293794663487685
+                        </details>
+                        <details open>
+                            <summary>To: <strong>Kriva polyna</strong></summary>
+                            <GrMapLocation />&nbsp; 018293794663487685
+                        </details>
+                    </div>
+
+                    <div className="hike-details__trail-info__pair">
+                        <p><GiPathDistance />&nbsp; 35km</p>
+                        <p><GiMountainRoad />&nbsp; elevation: 900m</p>
+                    </div>
+
+                    <div className="hike-details__trail-info__pair">
+                        <p>season:&nbsp; Summer</p>
+                        <ul>suitable for:
+                            <li><p>hiking</p></li>
+                            <li><p>mountain biking</p></li>
+                        </ul>
+                    </div>
+
+                    <p className="hike-details__trail-info__info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel ea dignissimos eligendi, est quas mollitia. Reprehenderit facere necessitatibus perspiciatis veritatis itaque praesentium voluptates eligendi ratione molestias delectus optio nemo quibusdam distinctio quia, dolorum minima dolorem eveniet cumque minus. Veritatis ex quo quod error accusamus adipisci dolorem magni. Amet repudiandae totam repellendus, iure aspernatur molestiae ea architecto aliquam quam facere perferendis minima voluptatum eveniet at necessitatibus ad suscipit. Quia, nemo sunt recusandae ex fugit, velit sint tempora ratione ipsa voluptatibus aliquid asperiores quo ullam est! Accusantium quaerat alias quidem nobis repudiandae voluptatibus rem tempore explicabo ipsa magni! Aspernatur sunt culpa vero tempora perspiciatis sequi consectetur, officia doloremque fugiat saepe earum officiis distinctio deserunt quos sint, explicabo iste similique repellendus error perferendis dicta laboriosam, praesentium non molestias. Velit quidem incidunt facere voluptas!</p>
                 </section>
 
                 <section className="hike-details__comments">
