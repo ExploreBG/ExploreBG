da9e9cf5e3cdc9d152d1ceb94af9b5c5f211a3f4
IVO-PC\Ivo <ivailo.st.ivanov@gmail.com>
2024-05-11 15:42:12 +0300

    Deploy fix - add unstable_setRequestLocale to About page.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { unstable_setRequestLocale } from 'next-intl/server';
 
 import './about.scss';
 import Layout from '@/components/Layout/Layout';
 
-interface AboutProps { }
+interface AboutProps {
+    params: { locale: string }
+}
 
-const About: React.FC<AboutProps> = () => {
+const About: React.FC<AboutProps> = ({ params: { locale } }) => {
+    unstable_setRequestLocale(locale);
+
     return (
         <Layout>
             <main>About page</main>
